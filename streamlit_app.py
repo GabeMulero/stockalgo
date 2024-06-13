ddfb04c8ebb4052a1fe4c22166fc17eb7917a05d
GabeMulero <gmulero@wgu.edu>
2024-06-13 18:24:31 -0500

    changed back?

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.markdown("# Stock Screener")
 n_rows, data = (Query()
  .select('name', 'close', 'volume', 'pre-market_volume', 'relative_volume_10d_calc', 'average_volume')
  .where(
-     Column('market_cap_basic').between(200_000_000, 1_000_000_000),
+     Column('market_cap_basic').between(1_000_000, 50_000_000),
      Column('relative_volume_10d_calc') > 1.2,
      Column('MACD.macd') >= Column('MACD.signal')
  )
